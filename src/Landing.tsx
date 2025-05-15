c070d254700dc3dc01ecf1b6784bdf76de6d16b5
Otso Saarinen <saarinenotso@gmail.com>
2025-05-15 22:10:36 +0300

    add membership features and recommendation section

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,16 +1,19 @@
 import "./Globals.css";
 import { Link } from "react-router";
-import { Github } from "lucide-react";
+import { Github, Moon, Sun, CircleCheck, Star } from "lucide-react";
 
 function Landing() {
 	return (
 		<>
 			<div className="font-jost bg-ghost-white flex min-h-screen max-w-screen flex-col items-center gap-12 py-25 text-neutral-800">
-				<div className="bg-tropical-indigo absolute top-0 flex w-full flex-row items-center justify-between p-3 font-semibold text-white">
-					<div className="ml-5">
+				<div className="bg-tropical-indigo fixed top-0 flex w-full flex-row items-center justify-between p-4 font-semibold text-white">
+					<div className="ml-5 flex flex-row gap-5">
 						<Link to="https://github.com/otsosaarinen/fiilis">
 							<Github />
 						</Link>
+						<div className="hover:cursor-pointer">
+							<Moon />
+						</div>
 					</div>
 					<div className="mr-5 flex flex-row gap-5">
 						<div className="hover:underline">
@@ -32,11 +35,11 @@ function Landing() {
 				<section className="flex flex-col items-center justify-center gap-5 text-5xl font-medium">
 					<div className="text-center">Membership types</div>
 					<div className="grid w-80 grid-cols-1 gap-5 lg:w-200 lg:grid-cols-2">
-						<div className="rounded-xl shadow-md shadow-neutral-800/20">
+						<div className="rounded-xl bg-white shadow-md shadow-neutral-800/20">
 							<div className="bg-periwinkle flex items-center justify-center rounded-t-xl p-1 text-lg">
 								Most popular subscription
 							</div>
-							<div className="flex flex-col items-center justify-center gap-2 rounded-b-xl bg-white p-2">
+							<div className="flex flex-col items-center justify-center gap-2 rounded-b-xl p-5">
 								<p className="text-4xl">Basic</p>
 								<p className="text-tropical-indigo text-xl">
 									Essential features
@@ -45,13 +48,31 @@ function Landing() {
 									4,99€
 								</p>
 								<p className="text-4xl">Free</p>
+								<ul className="flex flex-col gap-1 text-xl">
+									<li className="flex flex-row gap-1">
+										<CircleCheck color="#9381ff" /> Water
+										intake tracker
+									</li>
+									<li className="flex flex-row gap-1">
+										<CircleCheck color="#9381ff" />
+										Sleep quality tracker
+									</li>
+									<li className="flex flex-row gap-1">
+										<CircleCheck color="#9381ff" />
+										Quick mood check-ins
+									</li>
+									<li className="flex flex-row gap-1">
+										<CircleCheck color="#9381ff" />
+										Mood journal
+									</li>
+								</ul>
 							</div>
 						</div>
-						<div className="rounded-xl shadow-md shadow-neutral-800/20">
+						<div className="rounded-xl bg-white shadow-md shadow-neutral-800/20">
 							<div className="bg-periwinkle flex items-center justify-center rounded-t-xl p-1 text-lg">
 								Premium membership
 							</div>
-							<div className="flex flex-col items-center justify-center gap-2 rounded-b-xl bg-white p-2">
+							<div className="flex flex-col items-center justify-center gap-2 rounded-b-xl p-5">
 								<p className="text-4xl">Fiilis+</p>
 								<p className="text-tropical-indigo text-xl">
 									Advanced features
@@ -60,16 +81,49 @@ function Landing() {
 									49,95€
 								</p>
 								<p className="text-4xl">19,95€</p>
+								<ul className="flex flex-col gap-1 text-xl">
+									<li className="flex flex-row gap-1">
+										<CircleCheck color="#9381ff" />
+										Includes all Basic features
+									</li>
+									<li className="flex flex-row gap-1">
+										<CircleCheck color="#9381ff" /> AI-based
+										mood prediction trends
+									</li>
+									<li className="flex flex-row gap-1">
+										<CircleCheck color="#9381ff" />
+										Personalized wellness goals
+									</li>
+									<li className="flex flex-row gap-1">
+										<CircleCheck color="#9381ff" />
+										Exportable PDF wellbeing reports
+									</li>
+								</ul>
 							</div>
 						</div>
 					</div>
 				</section>
-				<div className="text-2xl">
+				<section className="flex flex-col items-center justify-center text-2xl">
+					<div className="flex flex-row">
+						<Star fill="#FFDF00" strokeWidth={2} />
+						<Star fill="#FFDF00" strokeWidth={2} />
+						<Star fill="#FFDF00" strokeWidth={2} />
+						<Star fill="#FFDF00" strokeWidth={2} />
+						<Star strokeWidth={2} />
+					</div>
+					<div>
+						<span className="text-tropical-indigo font-semibold">
+							80%
+						</span>{" "}
+						of our customers recommend us
+					</div>
+				</section>
+				<section className="text-center text-2xl">
 					<span className="text-tropical-indigo font-semibold hover:underline">
 						<Link to="/signup">Sign up</Link>
 					</span>{" "}
 					now and take control of your wellbeing.
-				</div>
+				</section>
 			</div>
 		</>
 	);
