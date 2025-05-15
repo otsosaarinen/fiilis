1f2f7b48e8dd070e147d61afce4643cf7a9eeb02
Otso Saarinen <saarinenotso@gmail.com>
2025-05-15 20:34:16 +0300

    add login & signup to routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Landing from "./Landing.tsx";
+import Login from "./Login.tsx";
+import Signup from "./Signup.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Landing />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>,
