0ff644b3b6cc8a6ffbed2ff2258d3c4509cce349
Otso Saarinen <saarinenotso@gmail.com>
2025-05-15 22:28:13 +0300

    separate header into own file

---
+++ src/Header.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router";
+import { Github, Moon } from "lucide-react";
+
+function Header() {
+	return (
+		<>
+			<div className="bg-tropical-indigo fixed top-0 flex w-full flex-row items-center justify-between p-4 font-semibold text-white">
+				<div className="ml-5 flex flex-row gap-5">
+					<Link to="https://github.com/otsosaarinen/fiilis">
+						<Github />
+					</Link>
+					<div className="hover:cursor-pointer">
+						<Moon />
+					</div>
+				</div>
+				<div className="mr-5 flex flex-row gap-5">
+					<div className="hover:underline">
+						<Link to="/login">Login</Link>
+					</div>
+					<div className="hover:underline">
+						<Link to="/signup">Sign up</Link>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default Header;
