3add8a71ca72148372d2ba5db607975ca1fe7232
Otso Saarinen <saarinenotso@gmail.com>
2025-05-15 20:17:14 +0300

    add color variables to landing page

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -3,25 +3,25 @@ import "./Globals.css";
 function Landing() {
 	return (
 		<>
-			<div className="font-jost flex min-h-screen max-w-screen flex-col items-center gap-12 py-20 text-neutral-800">
-				<h1 className="text-9xl font-semibold text-green-400">
+			<div className="font-jost bg-ghost-white flex min-h-screen max-w-screen flex-col items-center gap-12 py-20 text-neutral-800">
+				<h1 className="text-tropical-indigo text-9xl font-semibold">
 					Fiilis
 				</h1>
 				<h2 className="max-w-175 text-center text-2xl">
-					Track your physical and mental wellbeing with Fiilis. Gain
-					insights into your daily habits and improve your overall
-					wellbeing.
+					Track your physical and mental wellbeing with Fiilis. <br />
+					Gain insights into your daily habits and improve your
+					overall wellbeing.
 				</h2>
 				<section className="flex flex-col items-center justify-center gap-5 text-5xl font-medium">
 					<div className="text-center">Membership types</div>
 					<div className="grid w-80 grid-cols-1 gap-5 lg:w-200 lg:grid-cols-2">
 						<div>
-							<div className="flex items-center justify-center rounded-t-xl bg-green-400 p-1 text-lg">
+							<div className="bg-periwinkle flex items-center justify-center rounded-t-xl p-1 text-lg">
 								Most popular subscription
 							</div>
-							<div className="flex flex-col items-center justify-center gap-2 rounded-b-xl bg-neutral-50 p-2">
+							<div className="flex flex-col items-center justify-center gap-2 rounded-b-xl bg-white p-2">
 								<p className="text-4xl">Basic</p>
-								<p className="text-xl text-blue-400">
+								<p className="text-tropical-indigo text-xl">
 									Essential features
 								</p>
 								<p className="text-xl text-neutral-500 line-through">
@@ -31,12 +31,12 @@ function Landing() {
 							</div>
 						</div>
 						<div>
-							<div className="flex items-center justify-center rounded-t-xl bg-green-400 p-1 text-lg">
+							<div className="bg-periwinkle flex items-center justify-center rounded-t-xl p-1 text-lg">
 								Premium membership
 							</div>
-							<div className="flex flex-col items-center justify-center gap-2 rounded-b-xl bg-neutral-50 p-2">
+							<div className="flex flex-col items-center justify-center gap-2 rounded-b-xl bg-white p-2">
 								<p className="text-4xl">Fiilis+</p>
-								<p className="text-xl text-blue-400">
+								<p className="text-tropical-indigo text-xl">
 									Advanced features
 								</p>
 								<p className="text-xl text-neutral-500 line-through">
