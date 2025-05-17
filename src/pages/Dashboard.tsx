f22216af4ba17ef45756f622cfbffd297ffc7085
Otso Saarinen <saarinenotso@gmail.com>
2025-05-17 17:09:09 +0300

    create base dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,14 @@
+import Header from "../components/Header";
+
 function Dashboard() {
-	return <></>;
+	return (
+		<>
+			<div className="font-jost bg-ghost-white flex min-h-screen max-w-screen flex-col items-center pt-25 text-neutral-800">
+				<Header />
+				<div className="text-lg">Test dashboard</div>
+			</div>
+		</>
+	);
 }
 
 export default Dashboard;
