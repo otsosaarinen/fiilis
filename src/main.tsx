c0a2c1b41b44196e466dcd586ef5b0f3eeb8f0d8
Otso Saarinen <saarinenotso@gmail.com>
2025-05-17 16:45:47 +0300

    add dashboard to routes

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Landing from "./pages/Landing.tsx";
 import Login from "./pages/Login.tsx";
 import Signup from "./pages/Signup.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
 				<Route path="/" element={<Landing />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<Signup />} />
+				<Route path="/dashboard" element={<Dashboard />} />
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>,
