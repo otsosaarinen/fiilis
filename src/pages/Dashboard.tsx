9c3efa36b4d07fabf56107ef049f4fa3ee575826
Otso Saarinen <saarinenotso@gmail.com>
2025-05-17 17:28:56 +0300

    finish dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,7 +5,14 @@ function Dashboard() {
 		<>
 			<div className="font-jost bg-ghost-white flex min-h-screen max-w-screen flex-col items-center pt-25 text-neutral-800">
 				<Header />
-				<div className="text-lg">Test dashboard</div>
+				<div className="h-150 w-[95%] rounded-xl bg-white text-lg shadow-md shadow-neutral-800/20">
+					<div className="bg-periwinkle flex items-center justify-center rounded-t-xl p-1 font-medium">
+						Otso's dashboard
+					</div>
+					<div className="flex h-full items-center justify-center">
+						Content
+					</div>
+				</div>
 			</div>
 		</>
 	);
