13717ff970819708420018c997e2e1cb3603ebb9
Otso Saarinen <saarinenotso@gmail.com>
2025-05-29 15:27:56 +0300

    add api route for creating a new user

--- server/routes/createUser.ts
+++ server/routes/createUser.ts
@@ -1,3 +1,5 @@
+import bcrypt from "bcryptjs";
+
 import { Router } from "express";
 import { createUser } from "../../prisma/utils/queries";
 import { SubscriptionType } from "../../generated/prisma";
@@ -5,20 +7,29 @@ import { SubscriptionType } from "../../generated/prisma";
 const router = Router();
 
 router.post("/", async (req, res) => {
+	const firstName = req.body.firstName;
+	const lastName = req.body.lastName;
+	const email = req.body.email;
+	const password = req.body.password;
+	const createdAt = req.body.createdAt;
+	const subscription = req.body.subscription as SubscriptionType;
+
+	const salt = await bcrypt.genSalt(10);
+	const hash = await bcrypt.hash(password, salt);
+
 	try {
-		console.log(req.body);
 		const data = {
-			firstName: "test",
-			lastName: "test",
-			email: "test",
-			passwordHash: "123321",
-			createdAt: new Date("2025-05-29"),
-			subscription: SubscriptionType.Premium,
+			firstName: firstName,
+			lastName: lastName,
+			email: email,
+			passwordHash: hash,
+			createdAt: createdAt,
+			subscription: subscription,
 		};
 		await createUser(data);
-		res.status(200).json({ message: "Request received succesfully" });
+		res.status(200).json({ message: "User created succesfully" });
 	} catch (error) {
-		res.status(500).json({ message: "Error while processing the request" });
+		res.status(500).json({ message: "Error while creating an user" });
 		console.log(error);
 	}
 });
