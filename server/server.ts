c24113b016893db8e563cf0381d13ced678b5843
Otso Saarinen <saarinenotso@gmail.com>
2025-05-29 14:41:07 +0300

    entry file for the express server

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import router from "./routes/index";
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+app.use(express.json());
+app.use("/api", router);
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
