00d3b0649191c4bba96e2015bce9776898984049
Otso Saarinen <saarinenotso@gmail.com>
2025-05-29 15:11:31 +0300

    add cors

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import router from "./routes/index";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use("/api", router);
 
