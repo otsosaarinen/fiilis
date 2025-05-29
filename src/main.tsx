fe074eb574a5606d2b3566bdb13f9acf61183556
Otso Saarinen <saarinenotso@gmail.com>
2025-05-29 15:47:45 +0300

    add Layout to routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Layout from "./pages/Layout.tsx";
 import Landing from "./pages/Landing.tsx";
 import Login from "./pages/Login.tsx";
 import Signup from "./pages/Signup.tsx";
@@ -10,10 +11,12 @@ createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Landing />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<Signup />} />
-				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Landing />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/dashboard" element={<Dashboard />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>,
