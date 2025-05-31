68731dc69e8e781e54be22ec471ebe19b1e5514b
Otso Saarinen <saarinenotso@gmail.com>
2025-05-31 11:42:52 +0300

    add toast for succesfull login/account creation

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
-import { Snackbar } from "@mui/material";
+import { Snackbar, SnackbarContent } from "@mui/material";
+import { Alert } from "@mui/material";
+import type { SnackbarCloseReason } from "@mui/material";
 
 function Layout() {
+	const [userCreatedSnackbar, setUserCreatedSnackbar] = useState(false);
+
+	const handleClick = () => {
+		setUserCreatedSnackbar(true);
+	};
+
+	const handleClose = (
+		event: React.SyntheticEvent | Event,
+		reason?: SnackbarCloseReason,
+	) => {
+		if (reason === "clickaway") {
+			return;
+		}
+		setUserCreatedSnackbar(false);
+	};
+
 	return (
 		<>
 			<Snackbar
-				open={true}
+				open={userCreatedSnackbar}
 				autoHideDuration={5000}
+				onClose={handleClose}
 				anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-				message="Account succesfully created"
-			/>
+			>
+				<Alert
+					onClose={handleClose}
+					severity="success"
+					variant="filled"
+				>
+					Account succesfully created
+				</Alert>
+			</Snackbar>
 			<Outlet />
 		</>
 	);
