fef44635030368943f64a364aec7ad474f74521e
Otso Saarinen <saarinenotso@gmail.com>
2025-05-31 10:30:19 +0300

    add auth path to router

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import createUserRouter from "./createUser";
+import authenticateUserRouter from "./authUser";
 
 const router = Router();
 
 router.use("/user", createUserRouter);
+router.use("/auth", authenticateUserRouter);
 
 export default router;
