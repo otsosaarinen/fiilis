492d5362f131545cef4125c4c8d9477298cbd881
Otso Saarinen <saarinenotso@gmail.com>
2025-05-31 17:53:24 +0300

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -45,6 +45,7 @@ function Login() {
 			const response = await fetch("http://localhost:3000/api/auth", {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
+				credentials: "include",
 				body: JSON.stringify({
 					email: formData.email,
 					password: formData.password,
