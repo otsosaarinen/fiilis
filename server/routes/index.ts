f9b379c7e20ebbe647a952d0ccfe41d91ed30d9d
Otso Saarinen <saarinenotso@gmail.com>
2025-05-31 20:33:10 +0300

    add /dashboard for express to use

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import createUserRouter from "./createUser";
 import authenticateUserRouter from "./authUser";
+import dashboardRouter from "./dashboard";
 
 const router = Router();
 
 router.use("/user", createUserRouter);
 router.use("/auth", authenticateUserRouter);
+router.use("/dashboard", dashboardRouter);
 
 export default router;
