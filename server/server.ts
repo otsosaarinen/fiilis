35906c3b9abb37cbdd10fadf69ec505f2d9eb55d
Otso Saarinen <saarinenotso@gmail.com>
2025-05-31 20:11:10 +0300

    add cookie-parser to express entry file

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/index";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use("/api", router);
