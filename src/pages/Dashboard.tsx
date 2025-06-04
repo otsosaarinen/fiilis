d86084cc4581dc1e0824cc4d1c5ca6c1b02c3269
Otso Saarinen <saarinenotso@gmail.com>
2025-06-04 19:47:25 +0300

    create base for dashboard api call

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,19 +1,50 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 
 function Dashboard() {
+	const [authStatus, setAuthStatus] = useState<boolean>(true);
+	const [userInformation, setUserInformation] = useState({
+		firstName: null,
+		lastName: null,
+		email: null,
+		subscription: null,
+	});
+
+	useEffect(() => {
+		const fetchUserData = async () => {
+			try {
+				const response = await fetch(
+					"http://localhost:3000/api/dashboard",
+					{
+						method: "GET",
+					},
+				);
+				if (response.ok) {
+					console.log(response);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		};
+	}, []);
 	return (
 		<>
-			<div className="font-jost bg-ghost-white flex min-h-screen max-w-screen flex-col items-center pt-25 text-neutral-800">
-				<Header />
-				<div className="h-150 w-[95%] rounded-xl bg-white text-lg shadow-md shadow-neutral-800/20">
-					<div className="bg-periwinkle flex items-center justify-center rounded-t-xl p-1 font-medium">
-						Otso's dashboard
-					</div>
-					<div className="flex h-full items-center justify-center">
-						Content
+			{authStatus ? (
+				<div className="font-jost bg-ghost-white flex min-h-screen max-w-screen flex-col items-center pt-25 text-neutral-800">
+					<Header />
+					<div className="h-150 w-[95%] rounded-xl bg-white text-lg shadow-md shadow-neutral-800/20">
+						<div className="bg-periwinkle flex items-center justify-center rounded-t-xl p-1 font-medium">
+							{userInformation.firstName}'s dashboard
+						</div>
+						<div className="flex h-full flex-col items-center justify-center">
+							<div>{userInformation.email}</div>
+							<div>{userInformation.subscription}</div>
+						</div>
 					</div>
 				</div>
-			</div>
+			) : (
+				""
+			)}
 		</>
 	);
 }
