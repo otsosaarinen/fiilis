563a6215cc0fbae4b580cb7e8b0580097e6d8c06
Otso Saarinen <saarinenotso@gmail.com>
2025-06-04 20:02:47 +0300

    add logic for fetching user data based on id and sending it back as a response

--- server/routes/dashboard.ts
+++ server/routes/dashboard.ts
@@ -1,24 +1,40 @@
-import { Router } from "express";
-import { authMiddleware } from "./middleware/authMiddleware";
+import { Router, Response } from "express";
+import {
+	authMiddleware,
+	AuthenticatedRequest,
+} from "./middleware/authMiddleware";
+import { findById } from "../../prisma/utils/queries";
 
 const router = Router();
 
-router.get("/", authMiddleware, async (req, res) => {
-	try {
-		res.status(200).json({
-			message: "succesful",
-			user: {
-				firstName: "",
-				lastName: "",
-				email: "",
-				subscription: "",
-			},
-		});
-		return;
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: "Something went wrong" });
-	}
-});
+router.get(
+	"/",
+	authMiddleware,
+	async (req: AuthenticatedRequest, res: Response) => {
+		try {
+			const userId = req.user?.id;
+			const response = await findById(userId);
+
+			if (!response) {
+				res.status(404).json({ message: "User not found" });
+				return;
+			}
+
+			res.status(200).json({
+				message: "succesful",
+				user: {
+					firstName: response.firstName,
+					lastName: response.lastName,
+					email: response.email,
+					subscription: response.subscription,
+				},
+			});
+			return;
+		} catch (error) {
+			console.log(error);
+			res.status(500).json({ message: "Something went wrong" });
+		}
+	},
+);
 
 export default router;
